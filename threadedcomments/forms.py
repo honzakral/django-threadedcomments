c95e2371ec8701e26356c4f79a1ba120955211dc
floguy <floguy@60388069-7741-0410-adde-d57be00c0a97>
2008-01-05 23:17:48 +0000

    Too many changes to list\!
    git-svn-id: https://django-threadedcomments.googlecode.com/svn/trunk@7 60388069-7741-0410-adde-d57be00c0a97

---
+++ threadedcomments/forms.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import User
+from django import newforms as forms
+from models import ThreadedComment, Vote
+
+class ThreadedCommentForm(forms.ModelForm):
+    content_type = forms.IntegerField(widget = forms.HiddenInput)
+    object_id = forms.IntegerField(widget = forms.HiddenInput)
+    parent = forms.ModelChoiceField(ThreadedComment.objects.all(), required=False, widget = forms.HiddenInput)
+    user = forms.ModelChoiceField(User.objects.all(), widget = forms.HiddenInput)
+    
+    class Meta:
+        model = ThreadedComment
+        fields = ('content_type', 'object_id', 'parent', 'comment', 'user')
+
+class VoteForm(forms.ModelForm):
+    comment = forms.ModelChoiceField(ThreadedComment.objects.all(), widget = forms.HiddenInput)
+    vote = forms.ChoiceField(choices=Vote.VOTE_CHOICES)
+    user = forms.ModelChoiceField(User.objects.all(), widget = forms.HiddenInput)
+    class Meta:
+        model = Vote
--- threadedcomments/tests.py
+++ threadedcomments/tests.py
@@ -1,4 +1,6 @@
 """
+### MODEL TESTS ###
+
 >>> import datetime
 >>> from models import ThreadedComment, Vote, TestModel
 >>> from django.contrib.auth.models import User
@@ -118,4 +120,28 @@
          I'm a fanboy!
  What are we talking about?
  Post moderator addition.  Does it still work?
+
+### FORM TESTS ###
+
+>>> from forms import ThreadedCommentForm, VoteForm
+>>> data = {'parent' : comment1.parent, 'comment' : comment1.comment, 'object_id' : comment1.object_id, 'content_type' : comment1.content_type.id, 'user' : user.id}
+>>> form = ThreadedCommentForm(data, instance = comment1)
+>>> form.is_valid()
+True
+>>> form.cleaned_data
+{'parent': None, 'comment': u'This is fun!  This is very fun!', 'object_id': 1, 'content_type': 10, 'user': <User: user>}
+>>> print form
+<tr><th><label for="id_comment">Comment:</label></th><td><textarea id="id_comment" rows="10" cols="40" name="comment">This is fun!  This is very fun!</textarea><input type="hidden" name="content_type" value="10" id="id_content_type" /><input type="hidden" name="object_id" value="1" id="id_object_id" /><input type="hidden" name="parent" id="id_parent" /><input type="hidden" name="user" value="1" id="id_user" /></td></tr>
+
+>>> data = {'user' : user.id, 'comment' : comment1.id, 'vote' : '+1'}
+>>> form = VoteForm({})
+>>> form.is_valid()
+False
+>>> form.errors
+{'comment': [u'This field is required.'], 'vote': [u'This field is required.'], 'user': [u'This field is required.']}
+>>> form = VoteForm(data)
+>>> form.is_valid()
+True
+>>> form.save()
+<Vote: Vote object>
 """
