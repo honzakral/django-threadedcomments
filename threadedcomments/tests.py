12431ecc27f1ee1fd5049c43625a40772404ecaa
Eric Florenzano <floguy@gmail.com>
2008-01-11 05:24:43 +0000

    Added tests and test functionaltiy.

--- threadedcomments/tests.py
+++ threadedcomments/tests.py
@@ -1,16 +1,16 @@
 """
-### MODEL TESTS ###
-
+##################################
+### Model and Moderation Tests ###
+##################################
 >>> import datetime
 >>> from models import FreeThreadedComment, ThreadedComment, Vote, FreeVote, TestModel
 >>> from django.contrib.auth.models import User
 >>> from django.contrib.contenttypes.models import ContentType
 >>> from moderation import moderator, ThreadedCommentModerator
 
->>> topic = TestModel(name = "Test")
->>> topic.save()
->>> user = User.objects.create_user('user', 'floguy@gmail.com', 'password')
->>> user2 = User.objects.create_user('user2', 'floguy@gmail.com', 'password')
+>>> topic = TestModel.objects.create(name = "Test")
+>>> user = User.objects.create_user('user', 'floguy@gmail.com', password='password')
+>>> user2 = User.objects.create_user('user2', 'floguy@gmail.com', password='password')
 >>> comment1 = ThreadedComment.objects.create_for_object(
 ...     topic,
 ...     user = user,
@@ -120,4 +120,98 @@
          I'm a fanboy!
  What are we talking about?
  Post moderator addition.  Does it still work?
+
+############################
+### Views and URLs Tests ###
+############################
+>>> from django.core.urlresolvers import reverse
+>>> from django.test.client import Client
+
+>>> topic = TestModel.objects.create(name = "Test2")
+>>> content_type = ContentType.objects.get_for_model(topic)
+  ###########################################
+  ### FreeThreadedComments URLs Testsests ###
+  ###########################################
+>>> c = Client()
+
+>>> url = reverse('tc_free_comment', 
+...     kwargs={'content_type': content_type.id, 'object_id' : topic.id}
+... )
+>>> response = c.post(url, {'comment' : 'test', 'name' : 'eric', 'website' : 'http://www.eflorenzano.com/', 'email' : 'floguy@gmail.com', 'next' : '/'})
+>>> print response
+Content-Type: text/html; charset=utf-8
+Location: http://testserver/
+<BLANKLINE>
+<BLANKLINE>
+
+>>> url = reverse('tc_free_comment_ajax', 
+...     kwargs={'content_type': content_type.id, 'object_id' : topic.id,
+...         'ajax' : 'json'}
+... )
+>>> response = c.post(url, {'comment' : 'test', 'name' : 'eric', 'website' : 'http://www.eflorenzano.com/', 'email' : 'floguy@gmail.com', 'next' : '/'})
+>>> print response
+Content-Type: application/json
+<BLANKLINE>
+[{"pk": 2, "model": "threadedcomments.freethreadedcomment", "fields": {"website": "http:\/\/www.eflorenzano.com\/", "comment": "test", "name": "eric", "parent": null, "date_modified":\
+...
+
+>>> url = reverse('tc_free_comment_ajax', 
+...     kwargs={'content_type': content_type.id, 'object_id' : topic.id,
+...         'ajax' : 'xml'}
+... )
+>>> response = c.post(url, {'comment' : 'test', 'name' : 'eric', 'website' : 'http://www.eflorenzano.com/', 'email' : 'floguy@gmail.com', 'next' : '/'})
+>>> print response
+Content-Type: application/xml
+<BLANKLINE>
+<?xml version="1.0" encoding="utf-8"?>
+<django-objects version="1.0"><object pk="3" model="threadedcomments.freethreadedcomment"><field to="contenttypes.contenttype" name="content_type" rel="ManyToOneRel">8</field><field type="PositiveIntegerField" name="object_id">2</field><field to="threadedcomments.freethreadedcomment" name="parent" rel="ManyToOneRel"><None></None></field><field type="CharField" name="name">eric</field><field type="CharField" name="website">http://www.eflorenzano.com/</field><field type="CharField" name="email">floguy@gmail.com</field>\
+...
+
+>>> parent = FreeThreadedComment.objects.get_tree(topic)[0]
+
+>>> url = reverse('tc_free_comment_parent', 
+...     kwargs={'content_type': content_type.id, 'object_id' : topic.id, 
+...         'parent_id' : parent.id}
+... )
+>>> response = c.post(url, {'comment' : 'test', 'name' : 'eric', 'website' : 'http://www.eflorenzano.com/', 'email' : 'floguy@gmail.com', 'next' : '/'})
+>>> print response
+Content-Type: text/html; charset=utf-8
+Location: http://testserver/
+<BLANKLINE>
+<BLANKLINE>
+
+>>> url = reverse('tc_free_comment_parent_ajax', 
+...     kwargs={'content_type': content_type.id, 'object_id' : topic.id, 
+...         'parent_id' : parent.id, 'ajax' : 'json'}
+... )
+>>> response = c.post(url, {'comment' : 'test', 'name' : 'eric', 'website' : 'http://www.eflorenzano.com/', 'email' : 'floguy@gmail.com', 'next' : '/'})
+>>> print response
+Content-Type: application/json
+<BLANKLINE>
+[{"pk": 5, "model": "threadedcomments.freethreadedcomment", "fields": {"website": "http:\/\/www.eflorenzano.com\/", "comment": "test", "name": "eric", "parent": 1, "date_modified":\
+...
+
+>>> url = reverse('tc_free_comment_parent_ajax', 
+...     kwargs={'content_type': content_type.id, 'object_id' : topic.id, 
+...         'parent_id' : parent.id, 'ajax' : 'xml'}
+... )
+>>> response = c.post(url, {'comment' : 'test', 'name' : 'eric', 'website' : 'http://www.eflorenzano.com/', 'email' : 'floguy@gmail.com', 'next' : '/'})
+>>> print response
+Content-Type: application/xml
+<BLANKLINE>
+<?xml version="1.0" encoding="utf-8"?>
+<django-objects version="1.0"><object pk="6" model="threadedcomments.freethreadedcomment"><field to="contenttypes.contenttype" name="content_type" rel="ManyToOneRel">8</field><field type="PositiveIntegerField" name="object_id">2</field><field to="threadedcomments.freethreadedcomment" name="parent" rel="ManyToOneRel">1</field><field type="CharField" name="name">eric</field><field type="CharField" name="website">http://www.eflorenzano.com/</field><field type="CharField" name="email">floguy@gmail.com</field>\
+...
+
+
+  #######################################
+  ### ThreadedComments URLs Testsests ###
+  #######################################
+>>> User.objects.create_user('testuser', 'testuser@gmail.com', password='password')
+<User: testuser>
+>>> c = Client()
+>>> c.login(username='testuser', password='password')
+True
+>>> 1 == 2
+False
 """
--- threadedcomments/views.py
+++ threadedcomments/views.py
@@ -1,15 +1,17 @@
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.contrib.auth.decorators import login_required
+from django.contrib.contenttypes.models import ContentType
 from django.shortcuts import get_object_or_404
 from forms import FreeThreadedCommentForm, ThreadedCommentForm
 from models import ThreadedComment, FreeThreadedComment, Vote, FreeVote
+from utils import JSONResponse, XMLResponse
 
 def comment(request, content_type, object_id, parent_id=None, add_messages=True, ajax=False):
     form = ThreadedCommentForm(request.POST or None)
     if form.is_valid():
         new_comment = form.save(commit=False)
-        new_comment.ip_address = request.META['REMOTE_ADDR']
-        new_comment.content_type_id = int(content_type)
+        new_comment.ip_address = request.META.get('REMOTE_ADDR', None)
+        new_comment.content_type_id = get_object_or_404(ContentType, id = int(content_type))
         new_comment.object_id = int(object_id)
         new_comment.user = request.user
         if parent_id:
@@ -17,19 +19,20 @@ def comment(request, content_type, object_id, parent_id=None, add_messages=True,
         new_comment.save()
         if add_messages:
             request.user.message_set.create(message="Your message has been posted successfully.")
+        if ajax == 'json':
+            return JSONResponse([new_comment,])
+        elif ajax == 'xml':
+            return XMLResponse([new_comment,])
+        else:
+            # Determine next url
+            next = request.POST.get('next', request.GET.get('next', request.META.get('HTTP_REFERER', None)))
+            if not next or next == request.path:
+                next = '/'# No next was specified in POST or GET, will go to website root.
+            return HttpResponseRedirect(next)
     else:
         if add_messages:
             for error in form.errors:
                 request.user.message_set.create(message=error)
-    if ajax:
-        # TODO: decide whether simplejson is needed
-        return HttpResponse('{"comment_posted" : true}', mimetype="application/json")
-    else:
-        # Determine next url
-        next = request.POST.get('next', request.GET.get('next', request.META.get('HTTP_REFERER', None)))
-        if not next or next == request.path:
-            next = '/'# No next was specified in POST or GET, will go to website root.
-        return HttpResponseRedirect(next)
 
 comment = login_required(comment)
 
@@ -37,21 +40,24 @@ def free_comment(request, content_type, object_id, parent_id=None, ajax=False):
     form = FreeThreadedCommentForm(request.POST or None)
     if form.is_valid():
         new_comment = form.save(commit=False)
-        new_comment.ip_address = request.META['REMOTE_ADDR']
-        new_comment.content_type = int(content_type)
+        new_comment.ip_address = request.META.get('REMOTE_ADDR', None)
+        new_comment.content_type = get_object_or_404(ContentType, id = int(content_type))
         new_comment.object_id = int(object_id)
         if parent_id:
             new_comment.parent = get_object_or_404(FreeThreadedComment, id = int(parent_id))
         new_comment.save()
-    if ajax:
-        # TODO: decide whether simplejson is needed
-        return HttpResponse('{"comment_posted" : true}', mimetype="application/json")
+        if ajax == 'json':
+            return JSONResponse([new_comment,])
+        elif ajax == 'xml':
+            return XMLResponse([new_comment,])
+        else:
+            # Determine next url
+            next = request.POST.get('next', request.GET.get('next', request.META.get('HTTP_REFERER', None)))
+            if not next or next == request.path:
+                raise Http404 # No next url was supplied in GET or POST.
+            return HttpResponseRedirect(next)
     else:
-        # Determine next url
-        next = request.POST.get('next', request.GET.get('next', request.META.get('HTTP_REFERER', None)))
-        if not next or next == request.path:
-            raise Http404 # No next url was supplied in GET or POST.
-        return HttpResponseRedirect(next)
+        raise Http404
 
 def vote(request, comment, vote, free=False, ajax=False):
     # Parse the arguments into numerical data
@@ -74,9 +80,10 @@ def vote(request, comment, vote, free=False, ajax=False):
     )
     new_vote.save()
     
-    if ajax:
-        # TODO: decide whether simplejson is needed
-        return HttpResponse('{"comment" : %s, "vote" : %s}' % (comment, vote), mimetype="application/json")
+    if ajax == 'json':
+        return JSONResponse([new_vote,])
+    elif ajax == 'xml':
+        return XMLResponse([new_vote,])
     else:
         # Determine next url
         next = request.POST.get('next', request.GET.get('next', request.META.get('HTTP_REFERER', None)))
