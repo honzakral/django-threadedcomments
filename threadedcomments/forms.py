2db18d333422b754a48ebc7e5e5e7a0efdf3d927
floguy <floguy@60388069-7741-0410-adde-d57be00c0a97>
2008-01-18 03:38:06 +0000

    Added tests for auto_transform_markup and commented all template tags.
    git-svn-id: https://django-threadedcomments.googlecode.com/svn/trunk@25 60388069-7741-0410-adde-d57be00c0a97

--- threadedcomments/forms.py
+++ threadedcomments/forms.py
@@ -3,13 +3,18 @@ from django import newforms as forms
 from models import FreeThreadedComment, ThreadedComment
 
 class ThreadedCommentForm(forms.ModelForm):
-    
+    """
+    Form which can be used to validate data for a new ThreadedComment.
+    It consists of just one field: Comment.  I'm not e
+    """
     class Meta:
         model = ThreadedComment
-        fields = ('comment',)
+        fields = ('comment', 'markup')
 
 class FreeThreadedCommentForm(forms.ModelForm):
-    
+    """
+    Form which can be used to validate data for a new FreeThreadedComment.
+    """
     class Meta:
         model = FreeThreadedComment
-        fields = ('comment', 'name', 'website', 'email')
+        fields = ('comment', 'name', 'website', 'email', 'markup')
