d5979464933bbae3000cc851a349264607d892b8
Eric Florenzano <floguy@gmail.com>
2008-01-18 03:19:28 +0000

    Added tests for auto_transform_markup and commented all template tags.

--- threadedcomments/templatetags/threadedcommentstags.py
+++ threadedcomments/templatetags/threadedcommentstags.py
@@ -7,6 +7,9 @@ from threadedcomments.models import ThreadedComment, FreeThreadedComment
 from django import template
 
 def get_contenttype_kwargs(content_object):
+    """
+    Gets the basic kwargs necessary for almost all of the following tags.
+    """
     kwargs = {
         'content_type' : ContentType.objects.get_for_model(content_object).id,
         'object_id' : getattr(content_object, 'pk', getattr(content_object, 'id')),
@@ -14,6 +17,10 @@ def get_contenttype_kwargs(content_object):
     return kwargs
 
 def get_comment_url(content_object, parent=None):
+    """
+    Given an object and an optional parent, this tag gets the URL to POST to for the
+    creation of new ``ThreadedComment`` objects.
+    """
     kwargs = get_contenttype_kwargs(content_object)
     if parent:
         if not isinstance(parent, ThreadedComment):
@@ -24,6 +31,11 @@ def get_comment_url(content_object, parent=None):
         return reverse('tc_comment', kwargs=kwargs)
 
 def get_comment_url_ajax(content_object, parent=None, ajax_type='json'):
+    """
+    Given an object and an optional parent, this tag gets the URL to POST to for the
+    creation of new ``ThreadedComment`` objects.  It returns the latest created object
+    in the AJAX form of the user's choosing (json or xml).
+    """
     kwargs = get_contenttype_kwargs(content_object)
     kwargs.update({'ajax' : ajax_type})
     if parent:
@@ -35,18 +47,30 @@ def get_comment_url_ajax(content_object, parent=None, ajax_type='json'):
         return reverse('tc_comment_ajax', kwargs=kwargs)
 
 def get_comment_url_json(content_object, parent=None):
+    """
+    Wraps ``get_comment_url_ajax`` with ``ajax_type='json'``
+    """
     try:
         return get_comment_url_ajax(content_object, parent, ajax_type="json")
     except template.TemplateSyntaxError:
         raise template.TemplateSyntaxError, "get_comment_url_json requires its parent object to be of type ThreadedComment"
+    return ''
 
 def get_comment_url_xml(content_object, parent=None):
+    """
+    Wraps ``get_comment_url_ajax`` with ``ajax_type='xml'``
+    """
     try:
         return get_comment_url_ajax(content_object, parent, ajax_type="xml")
     except template.TemplateSyntaxError:
         raise template.TemplateSyntaxError, "get_comment_url_xml requires its parent object to be of type ThreadedComment"
+    return ''
 
 def get_free_comment_url(content_object, parent=None):
+    """
+    Given an object and an optional parent, this tag gets the URL to POST to for the
+    creation of new ``FreeThreadedComment`` objects.
+    """
     kwargs = get_contenttype_kwargs(content_object)
     if parent:
         if not isinstance(parent, FreeThreadedComment):
@@ -57,6 +81,11 @@ def get_free_comment_url(content_object, parent=None):
         return reverse('tc_free_comment', kwargs=kwargs)
 
 def get_free_comment_url_ajax(content_object, parent=None, ajax_type='json'):
+    """
+    Given an object and an optional parent, this tag gets the URL to POST to for the
+    creation of new ``FreeThreadedComment`` objects.  It returns the latest created object
+    in the AJAX form of the user's choosing (json or xml).
+    """
     kwargs = get_contenttype_kwargs(content_object)
     kwargs.update({'ajax' : ajax_type})
     if parent:
@@ -68,18 +97,53 @@ def get_free_comment_url_ajax(content_object, parent=None, ajax_type='json'):
         return reverse('tc_free_comment_ajax', kwargs=kwargs)
 
 def get_free_comment_url_json(content_object, parent=None):
+    """
+    Wraps ``get_free_comment_url_ajax`` with ``ajax_type='json'``
+    """
     try:
         return get_free_comment_url_ajax(content_object, parent, ajax_type="json")
     except template.TemplateSyntaxError:
         raise template.TemplateSyntaxError, "get_free_comment_url_json requires its parent object to be of type FreeThreadedComment"
+    return ''
 
 def get_free_comment_url_xml(content_object, parent=None):
+    """
+    Wraps ``get_free_comment_url_ajax`` with ``ajax_type='xml'``
+    """
     try:
         return get_free_comment_url_ajax(content_object, parent, ajax_type="xml")
     except template.TemplateSyntaxError:
         raise template.TemplateSyntaxError, "get_free_comment_url_xml requires its parent object to be of type FreeThreadedComment"
+    return ''
+
+def auto_transform_markup(comment):
+    """
+    Given a comment (``ThreadedComment`` or ``FreeThreadedComment``), this tag
+    looks up the markup type of the comment and formats the output accordingly.
+    """
+    try:
+        from django.contrib.markup.templatetags import markup
+        from django.utils.html import escape
+        from threadedcomments.models import MARKDOWN, TEXTILE, REST, HTML, PLAINTEXT
+        if comment.markup == MARKDOWN:
+            return markup.markdown(comment.comment)
+        elif comment.markup == TEXTILE:
+            return markup.textile(comment.comment)
+        elif comment.markup == REST:
+            return markup.restructuredtext(comment.comment)
+        elif comment.markup == HTML:
+            return mark_safe(force_unicode(comment.comment))
+        elif comment.markup == PLAINTEXT:
+            return escape(comment.comment)
+    except ImportError:
+        # Not marking safe, in case tag fails and users input malicious code.
+        return force_unicode(comment.comment)
 
 def do_get_threaded_comment_tree(parser, token):
+    """
+    Gets a tree (list of objects ordered by preorder tree traversal, and with an
+    additional ``depth`` integer attribute annotated onto each ``ThreadedComment``.
+    """
     try:
         split = token.split_contents()
     except ValueError:
@@ -87,6 +151,10 @@ def do_get_threaded_comment_tree(parser, token):
     return CommentTreeNode(split[2], split[4])
 
 def do_get_free_threaded_comment_tree(parser, token):
+    """
+    Gets a tree (list of objects ordered by traversing tree in preorder, and with an
+    additional ``depth`` integer attribute annotated onto each ``FreeThreadedComment.``
+    """
     try:
         split = token.split_contents()
     except ValueError:
@@ -111,23 +179,6 @@ class FreeCommentTreeNode(template.Node):
         context[self.context_name] = FreeThreadedComment.public.get_tree(content_object)
         return ''
 
-def auto_transform_markup(comment):
-    try:
-        from django.contrib import markup
-        from models import MARKDOWN, TEXTILE, REST, HTML, PLAINTEXT
-        if comment.markup == MARKDOWN:
-            return markup.markdown(comment.comment)
-        elif comment.markup == TEXTILE:
-            return markup.textile(comment.comment)
-        elif comment.markup == REST:
-            return markup.restructuredtext(comment.comment)
-        elif comment.markup == HTML:
-            return mark_safe(force_unicode(comment.comment))
-        elif comment.markup == PLAINTEXT:
-            return force_unicode(comment.comment)
-    except ImportError:
-        return force_unicode(comment.comment)
-
 register = template.Library()
 register.simple_tag(get_comment_url)
 register.simple_tag(get_comment_url_json)
@@ -135,7 +186,6 @@ register.simple_tag(get_comment_url_xml)
 register.simple_tag(get_free_comment_url)
 register.simple_tag(get_free_comment_url_json)
 register.simple_tag(get_free_comment_url_xml)
-
 register.simple_tag(auto_transform_markup)
 
 register.tag('get_threaded_comment_tree', do_get_threaded_comment_tree)
