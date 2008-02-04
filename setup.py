1e596b77f2e702a370620506c2f6a8554d220db4
floguy <floguy@60388069-7741-0410-adde-d57be00c0a97>
2008-02-04 09:28:42 +0000

    Forgot to increment version number in setup.py
    git-svn-id: https://django-threadedcomments.googlecode.com/svn/trunk@68 60388069-7741-0410-adde-d57be00c0a97

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 kwargs = {
     'name' : 'threadedcomments',
-    'version' : '0.1.1',
+    'version' : '0.2',
     'description' : 'A simple yet flexible threaded commenting system.',
     'author' : 'Eric Florenzano',
     'author_email' : 'floguy@gmail.com',
