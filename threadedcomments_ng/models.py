133a263c26ed376cc5b168f25e27bd050f875c9c
Honza Kral <Honza.Kral@gmail.com>
2009-03-31 03:36:57 -0500

    Initial working version

---
+++ threadedcomments_ng/models.py
@@ -0,0 +1,91 @@
+from django.db import models
+
+DIGITS = 10
+SEPARATOR = '/'
+
+
+class CommentManager(models.Manager):
+    def iter_tree(self):
+        it = self.all().iterator()
+
+        old = it.next()
+        old.open = 1
+        last = []
+        for c in it:
+            if old.last_child_id:
+                last.append(old.last_child_id)
+
+            if c.pk in last:
+                c.last = True
+
+            if c.level > old.level:
+                c.open = 1
+
+            else: # c.level <= old.level
+                old.close = old.level - c.level
+                if old.root != c.root:
+                    old.close += 1
+                    last = []
+                    c.open = 1
+            yield old
+            old = c
+        old.close = old.level
+        yield old
+
+    def pprint(self):
+        old = None
+        for c in self.iter_tree():
+            if c.level == getattr(old, 'level', -1):
+                print '</li>'
+
+            if getattr(c, 'open', False):
+                print '<ul>'
+
+            if getattr(c, 'last', False):
+                print '<li class="last">'
+            else:
+                print '<li>'
+
+            print '%s' % c
+
+            for x in range(getattr(c, 'close', 0)):
+                print '</li>'
+                print '</ul>'
+
+            old = c
+
+
+class Comment(models.Model):
+    parent = models.ForeignKey('self', null=True, blank=True, related_name='child_set')
+    last_child = models.ForeignKey('self', null=True, blank=True)
+    tree_path = models.TextField(editable=False)
+
+    objects = CommentManager()
+
+    def __unicode__(self):
+        return self.tree_path
+
+    class Meta:
+        ordering = ('tree_path',)
+
+    @property
+    def level(self):
+        return len(self.tree_path.split(SEPARATOR))
+
+    @property
+    def root(self):
+        return self.tree_path.split(SEPARATOR)[0]
+
+    def save(self, force_update=False, force_insert=False):
+        super(Comment, self).save(force_update=force_update, force_insert=force_insert)
+
+        tp = ''
+        if self.parent:
+            tp = self.parent.tree_path + SEPARATOR
+            self.parent.last_child = self
+            self.__class__.objects.filter(pk=self.parent_id).update(last_child=self)
+            
+        tree_path = tp + (DIGITS*'0'+str(self.pk))[-DIGITS:]
+        self.tree_path = tree_path
+        self.__class__.objects.filter(pk=self.pk).update(tree_path=tree_path)
+
