8c0e29c6173efd7f4ca63f68b0f9f1ea0a15681f
Eric Florenzano <floguy@gmail.com>
2009-03-31 15:36:32 -0500

    Removed the level field and added a path (ancestry) field.

--- threadedcomments/models.py
+++ threadedcomments/models.py
@@ -1,13 +1,23 @@
 from django.db import models
 from django.contrib.comments.models import Comment
+from django.conf import settings
+
+PATH_SEPARATOR = getattr(settings, 'COMMENT_PATH_SEPARATOR', '/')
+PATH_DIGITS = getattr(settings, 'COMMENT_PATH_DIGITS', 10)
 
 class ThreadedComment(Comment):
     parent = models.ForeignKey('self', null=True, blank=True, default=None,
         related_name='children')
-    level  = models.IntegerField(default=0)
-
-    def save(self, force_insert=False, force_update=False):
+    path = models.TextField(null=True, blank=True, db_index=True)
+    
+    def _get_depth(self):
+        return len(self.path.split(PATH_SEPARATOR)) - 1
+    depth = property(_get_depth)
+    
+    def save(self, *args, **kwargs):
+        super(ThreadedComment, self).save(*args, **kwargs)
+        path_list = [unicode(self.pk).zfill(PATH_DIGITS)]
         if self.parent:
-            self.level = self.parent.level + 1
-        super(ThreadedComment, self).save(force_insert=force_insert,
-            force_update=force_update)
+            path_list.append(self.parent.path)
+        self.path = PATH_SEPARATOR.join(path_list)
+        super(ThreadedComment, self).save(*args, **kwargs)
