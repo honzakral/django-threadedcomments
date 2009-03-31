f321b7605b20abdca012d3da5f65b1bdc042a366
Eric Florenzano <floguy@gmail.com>
2009-03-31 15:53:50 -0500

    Renamed path to tree_path

--- threadedcomments/models.py
+++ threadedcomments/models.py
@@ -8,16 +8,16 @@ PATH_DIGITS = getattr(settings, 'COMMENT_PATH_DIGITS', 10)
 class ThreadedComment(Comment):
     parent = models.ForeignKey('self', null=True, blank=True, default=None,
         related_name='children')
-    path = models.TextField(null=True, blank=True, db_index=True)
+    tree_path = models.TextField(null=True, blank=True, db_index=True)
     
     def _get_depth(self):
-        return len(self.path.split(PATH_SEPARATOR))
+        return len(self.tree_path.split(PATH_SEPARATOR))
     depth = property(_get_depth)
     
     def save(self, *args, **kwargs):
         super(ThreadedComment, self).save(*args, **kwargs)
         path_list = [unicode(self.pk).zfill(PATH_DIGITS)]
         if self.parent:
-            path_list.append(self.parent.path)
-        self.path = PATH_SEPARATOR.join(path_list)
+            path_list.append(self.parent.tree_path)
+        self.tree_path = PATH_SEPARATOR.join(path_list)
         super(ThreadedComment, self).save(*args, **kwargs)
