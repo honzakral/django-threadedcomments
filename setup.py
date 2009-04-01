6cb9219c350dc0ad38a8514c9f8c20dba6c839ac
Eric Florenzano <floguy@gmail.com>
2009-03-31 22:49:51 -0500

    Converted setup.py to setuptools

--- setup.py
+++ setup.py
@@ -1,23 +1,32 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 kwargs = {
-    'name' : 'django-threadedcomments',
-    'version' : '0.6',
-    'description' : 'A simple yet flexible threaded commenting system.',
-    'author' : 'Eric Florenzano',
-    'author_email' : 'floguy@gmail.com',
-    'url' : 'http://code.google.com/p/django-threadedcomments/',
-    'packages' : ['threadedcomments',
-                  'threadedcomments.templatetags',
-                  'threadedcomments.management',
-                  'threadedcomments.management.commands'],
-    'classifiers' : ['Development Status :: 4 - Beta',
-                     'Environment :: Web Environment',
-                     'Intended Audience :: Developers',
-                     'License :: OSI Approved :: BSD License',
-                     'Operating System :: OS Independent',
-                     'Programming Language :: Python',
-                     'Topic :: Utilities'],
+    'name': 'django-threadedcomments',
+    'version': '0.9',
+    'description': 'A simple yet flexible threaded commenting system.',
+    'author': 'Eric Florenzano',
+    'author_email': 'floguy@gmail.com',
+    'url': 'http://code.google.com/p/django-threadedcomments/',
+    'keywords': 'django,pinax,comments',
+    'license': 'BSD',
+    'packages': [
+        'threadedcomments',
+        'threadedcomments.templatetags',
+        'threadedcomments.management',
+        'threadedcomments.management.commands',
+    ],
+    'include_package_data': True,
+    'zip_safe': False,
+    'install_requires': ['setuptools'],
+    'classifiers': [
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities',
+    ],
 }
 from sys import version_info
 if version_info[1] >= 4:
