fa357c498d1f7c5835fd5e4a32c49033abf6ddf8
Jannis Leidel <jannis@leidel.info>
2009-08-02 16:54:16 +0200

    Updated setup.py to include all package data

--- setup.py
+++ setup.py
@@ -1,25 +1,36 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
-kwargs = {
-    'name' : 'django-threadedcomments',
-    'version' : '0.5.1',
-    'description' : 'A simple yet flexible threaded commenting system.',
-    'author' : 'Eric Florenzano',
-    'author_email' : 'floguy@gmail.com',
-    'url' : 'http://code.google.com/p/django-threadedcomments/',
-    'packages' : ['threadedcomments',
-                  'threadedcomments.templatetags',
-                  'threadedcomments.management',
-                  'threadedcomments.management.commands'],
-    'classifiers' : ['Development Status :: 4 - Beta',
-                     'Environment :: Web Environment',
-                     'Intended Audience :: Developers',
-                     'License :: OSI Approved :: BSD License',
-                     'Operating System :: OS Independent',
-                     'Programming Language :: Python',
-                     'Topic :: Utilities'],
-}
-from sys import version_info
-if version_info[1] >= 4:
-    kwargs['package_data'] = {'threadedcomments' : ['templates/comment_utils/*.txt','templates/threadedcomments/*.html','templates/threadedcomments_base.html']}
-setup(**kwargs)
+setup(
+    name='django-threadedcomments',
+    version='0.5.2',
+    description='A simple yet flexible threaded commenting system.',
+    author='Eric Florenzano',
+    author_email='floguy@gmail.com',
+    url='http://code.google.com/p/django-threadedcomments/',
+    keywords='django,pinax,comments',
+    license='BSD',
+    packages=[
+        'threadedcomments',
+        'threadedcomments.templatetags',
+        'threadedcomments.management',
+        'threadedcomments.management.commands',
+    ],
+    package_data={
+        'threadedcomments' : [
+            'templates/comment_utils/*.txt',
+            'templates/threadedcomments/*.html',
+            'templates/threadedcomments_base.html'
+        ],
+    },
+    include_package_data=True,
+    zip_safe=False,
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities',
+    ],
+)
