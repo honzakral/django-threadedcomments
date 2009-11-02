0e49e6c5517ac83eeb8c9a2923ea9ac33601dac2
Honza Kral <honza.kral@gmail.com>
2009-11-02 19:45:01 +0100

    less verbose tests

--- tests/runtests.py
+++ tests/runtests.py
@@ -9,6 +9,6 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 from django.test.simple import run_tests
 
 if __name__ == "__main__":
-    failures = run_tests(['threadedcomments', 'comments'], verbosity=9)
+    failures = run_tests(['threadedcomments', 'comments'], verbosity=1)
     if failures:
         sys.exit(failures)
