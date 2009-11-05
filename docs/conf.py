9bf0e91cfdb6844a1a97effe5033622b74bb81b5
Honza Kral <honza.kral@gmail.com>
2009-11-05 14:04:59 +0100

    Added basic sphinx conf

---
+++ docs/conf.py
@@ -0,0 +1,8 @@
+extensions = ['sphinx.ext.autodoc']
+templates_path = ['.templates']
+source_suffix = '.txt'
+master_doc = 'index'
+
+# General information about the project.
+project = u'Django threaded comments'
+
