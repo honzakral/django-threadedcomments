a85b3cffb83dd9db19dff0142008cbf41a85f8d3
Honza Kral <honza.kral@gmail.com>
2009-11-07 18:20:31 +0100

    Removed admin actions that have been suuplied by django's built-in comments app

--- threadedcomments/admin.py
+++ threadedcomments/admin.py
@@ -4,24 +4,6 @@ from django.contrib.comments.admin import CommentsAdmin
 
 from threadedcomments.models import ThreadedComment
 
-def remove_comment(modeladmin, request, queryset):
-    queryset.update(is_removed=True)
-remove_comment.short_description = _("Mark selected comments as removed")
- 
-def un_remove_comment(modeladmin, request, queryset):
-    queryset.update(is_removed=False)
-un_remove_comment.short_description = _("Mark selected comments as not removed")
- 
-def public_comment(modeladmin, request, queryset):
-    queryset.update(is_public=True)
-public_comment.short_description = _("Mark selected comments as published")
- 
-def un_public_comment(modeladmin, request, queryset):
-    queryset.update(is_public=False)
-un_public_comment.short_description = _("Mark selected comments as unpublished")
- 
- 
-
 class ThreadedCommentsAdmin(CommentsAdmin):
     fieldsets = (
         (None,
@@ -41,7 +23,6 @@ class ThreadedCommentsAdmin(CommentsAdmin):
     list_display = ('name', 'title', 'content_type', 'object_pk', 'parent', 'ip_address', 'submit_date', 'is_public', 'is_removed')
     search_fields = ('title', 'comment', 'user__username', 'user_name', 'user_email', 'user_url', 'ip_address')
     raw_id_fields = ("parent",)
-    actions = CommentsAdmin.actions + [remove_comment, un_remove_comment, public_comment, un_public_comment]
 
 admin.site.register(ThreadedComment, ThreadedCommentsAdmin)
 
