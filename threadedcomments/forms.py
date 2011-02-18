6f97065448b4976b8d3d2c49fb557ac56375c60c
Honza Kral <honza.kral@gmail.com>
2011-02-17 23:17:18 -0800

    title field label doesn't get translated Fixes #22
    Thanks sq9mev

--- threadedcomments/forms.py
+++ threadedcomments/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.comments.forms import CommentForm
 from django.conf import settings
 from django.utils.hashcompat import sha_constructor
+from django.utils.translation import ugettext_lazy as _
 
 from threadedcomments.models import ThreadedComment
 
@@ -10,7 +11,7 @@ class ThreadedCommentForm(CommentForm):
 
     def __init__(self, target_object, parent=None, data=None, initial=None):
         self.base_fields.insert(
-                self.base_fields.keyOrder.index('comment'), 'title',
+                self.base_fields.keyOrder.index('comment'), _('title'),
                 forms.CharField(
                     required=False,
                     max_length=getattr(settings, 'COMMENTS_TITLE_MAX_LENGTH', 255)
