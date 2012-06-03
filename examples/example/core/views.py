77e21098e057c35fb61df20fe050851c420687e6
Diederik van der Boor <vdboor@edoburu.nl>
2012-06-03 21:26:44 +0200

    Using get_object_or_404 in the example
    Nicely suggested by @HonzaKral in the comments of pull #39

--- examples/example/core/views.py
+++ examples/example/core/views.py
@@ -1,5 +1,5 @@
 from core.models import Message
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404
 from django.template.context import RequestContext
 from django.views.decorators.csrf import csrf_protect
 
@@ -14,6 +14,6 @@ def home(request):
 @csrf_protect
 def message(request, id):
     context = {
-        'message': Message.objects.get(id=id),
+        'message': get_object_or_404(Message, pk=id),
     }
     return render_to_response('core/message.html', context, context_instance=RequestContext(request))
