8232327be9751e5d3206c7a60dec2b877c504122
Diederik van der Boor <vdboor@edoburu.nl>
2015-04-14 00:47:03 +0200

    Add locale folder for translations

--- setup.py
+++ setup.py
@@ -2,7 +2,19 @@
 from setuptools import setup, find_packages
 from os import path
 import codecs
+import os
 import re
+import sys
+
+
+# When creating the sdist, make sure the django.mo file also exists:
+if 'sdist' in sys.argv or 'develop' in sys.argv:
+    os.chdir('threadedcomments')
+    try:
+        from django.core import management
+        management.call_command('compilemessages', stdout=sys.stderr, verbosity=1)
+    finally:
+        os.chdir('..')
 
 
 def read(*parts):
