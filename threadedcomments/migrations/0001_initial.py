a0c8a24fa44bc260ea8cdedfee760ba64d3072a3
Diederik van der Boor <vdboor@edoburu.nl>
2017-02-19 20:09:45 +0100

    Fix Django 1.7 migration support for real

--- threadedcomments/migrations/0001_initial.py
+++ threadedcomments/migrations/0001_initial.py
@@ -23,7 +23,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='ThreadedComment',
             fields=[
-                ('comment_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='django_comments.Comment')),
+                ('comment_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to=BASE_APP + '.Comment')),
                 ('title', models.TextField(verbose_name='Title', blank=True)),
                 ('tree_path', models.CharField(verbose_name='Tree path', max_length=500, editable=False, db_index=is_index)),
                 ('last_child', models.ForeignKey(on_delete=django.db.models.deletion.SET_NULL, verbose_name='Last child', blank=True, to='threadedcomments.ThreadedComment', null=True)),
